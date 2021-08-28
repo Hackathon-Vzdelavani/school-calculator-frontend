e41d109c701449685d57dc946e5f74b221d11a5f
Pavol Hejný <me@pavolhejny.com>
2021-08-28 10:54:43 +0000

    Ranges

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <form>form</form>
-    </>
-  );
+    return (
+        <div className="App">
+            {[1, 2, 3, 5, 6, 7, 8, 9, 10].map((key) => (
+                <div key={key}>
+                    Matematika: <input type="range" />
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
