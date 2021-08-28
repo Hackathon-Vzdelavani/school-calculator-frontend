42d9451d76cd139b52424d00bf25cc852d11574f
Pavol Hejný <me@pavolhejny.com>
2021-08-28 14:02:41 +0000

    UX of choose

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,14 @@ export function App() {
             <div>
                 {Object.entries(COURSES).map(([key, courseName]) => (
                     <div key={key}>
-                        <div style={{ display: 'inline-block', width: 500 }}>
-                            {courseName} ({key})
+                        <div
+                            style={{
+                                display: 'inline-block',
+                                textAlign: 'left',
+                                width: 400,
+                            }}
+                        >
+                            {courseName} <i>({key})</i>
                         </div>
                         <input
                             type="range"
@@ -33,7 +39,7 @@ export function App() {
                 ))}
             </div>
 
-            <div style={{ position: 'fixed', right: 0 }}>
+            <div style={{ position: 'fixed', right: 10, textAlign: 'left' }}>
                 {Object.entries(FACULTIES)
                     .map(([key, faculty]) => ({
                         key,
@@ -46,7 +52,15 @@ export function App() {
                             0,
                         ),
                     }))
-                    .sort(({ match: a }, { match: b }) => (a < b ? 1 : -1))
+                    .sort(({ match: a }, { match: b }) => {
+                        if (a < b) {
+                            return 1;
+                        } else if (a > b) {
+                            return -1;
+                        } else {
+                            return Math.random() > 0.5 ? 1 : -1;
+                        }
+                    })
                     .map(({ key, faculty, match }, i) => (
                         <div
                             key={key}
@@ -54,7 +68,12 @@ export function App() {
                                 // alert(match);
                             }}
                         >
-                            {i + 1}) {faculty.name}
+                            {i + 1}) {faculty.name}{' '}
+                            {match === 0 ? (
+                                ''
+                            ) : (
+                                <i>({Math.round(match * 10)})</i>
+                            )}
                         </div>
                     ))}
             </div>
