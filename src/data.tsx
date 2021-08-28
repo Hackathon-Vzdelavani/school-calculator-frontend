1586293802656e8f3f2f2bd9e9da4f33177771d4
Pavol Hejný <me@pavolhejny.com>
2021-08-28 11:26:25 +0000

    Data structure

--- src/data.tsx
+++ src/data.tsx
@@ -1,2 +1,13 @@
-export const SUBJECTS = { M: 'Mathematics' };
-export const SCHOOLS = { KFY: {} };
+export const SUBJECTS: Record<string, string> = { FYEF1: 'Mathematics' };
+
+export const SCHOOLS: Record<
+    string,
+    { title: string; subjects: Record<keyof typeof SUBJECTS, number> }
+> = {
+    KFY: {
+        title: 'Fakulta blblalba',
+        subjects: {
+            FYEF1: 12,
+        },
+    },
+};
